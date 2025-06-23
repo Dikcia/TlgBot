97551965bbd848d35ff4f5b0b08cff3f1a7c04c8
Dikcia <dikcia1964@gmail.com>
2025-06-23 13:30:33 -0400

    4files
    Commit4 files

--- main.py
+++ main.py
@@ -3,14 +3,12 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-# Виводимо все в лог
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.DEBUG  # DEBUG замість INFO
+    level=logging.INFO
 )
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    logging.info("Команда /start отримана")
     await update.message.reply_text("Привіт! Я бот і працюю на PTB 20+ ✅")
 
 if __name__ == '__main__':
@@ -18,7 +16,6 @@ if __name__ == '__main__':
     if not TOKEN:
         print("❌ BOT_TOKEN не встановлено!")
     else:
-        logging.info("Запускаємо бота…")
         app = ApplicationBuilder().token(TOKEN).build()
         app.add_handler(CommandHandler("start", start))
         app.run_polling()
